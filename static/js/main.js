e17661f4512d2d780dd9bad4b9dec8cefa07b380
Blake <blake@blakeowens.com>
2020-07-11 12:12:38 -0700

    Fix Safari issue, remove fonts

--- static/js/main.js
+++ static/js/main.js
@@ -1,56 +1,47 @@
-(function() {
-  class Main {
-    constructor() {
-      this.bindEvents();
-      svg4everybody();
-      retinajs();
-    }
-
-    bindEvents = () => {
-      const projectLinks = document.querySelectorAll('.js-open-project');
-      const closeLinks = document.querySelectorAll('.js-close');
-      const overlays = document.querySelectorAll('.overlay');
-
-      projectLinks.forEach((el) => {
-        el.addEventListener('click', this.onClickOpenProject, true);
-      });
-
-      closeLinks.forEach((el) => {
-        el.addEventListener('click', this.onClickClose, true);
-      });
-
-      // Fix for weird Safari scroll issue
-      overlays.forEach((el) => {
-        el.addEventListener('webkitTransitionEnd', function() {
-          this.style.overflowY = 'hidden';
-          setTimeout(() => { this.style.overflowY = 'auto'; });
-        });
-      });
-    }
-
-    onClickOpenProject = (event) => {
-      event.preventDefault();
-
-      const el = event.currentTarget;
-      const projectName = el.getAttribute('href').replace('#', '');
-      const overlay = document.querySelector(`[data-project="${projectName}"]`);
-
-      overlay.classList.add('is-shown');
-    }
-
-    onClickClose = (event) => {
-      event.preventDefault();
-
-      const el = event.currentTarget;
-      const projectName = el.getAttribute('data-target');
-      const overlay = document.querySelector(`[data-project="${projectName}"]`);
-
-      overlay.classList.remove('is-shown');
-      
-      setTimeout(() => { overlay.scrollTop = 0; }, 750);
-    }
+class Main {
+  constructor() {
+    this.bindEvents();
+    svg4everybody();
+    retinajs();
   }
 
-  new Main();
+  bindEvents() {
+    const projectLinks = document.querySelectorAll('.js-open-project');
+    const closeLinks = document.querySelectorAll('.js-close');
+    const overlays = document.querySelectorAll('.overlay');
 
+    projectLinks.forEach((el) => {
+      el.addEventListener('click', this.onClickOpenProject, true);
+    });
+
+    closeLinks.forEach((el) => {
+      el.addEventListener('click', this.onClickClose, true);
+    });
+  }
+
+  onClickOpenProject(event) {
+    event.preventDefault();
+
+    const el = event.currentTarget;
+    const projectName = el.getAttribute('href').replace('#', '');
+    const overlay = document.querySelector(`[data-project="${projectName}"]`);
+
+    overlay.classList.add('is-shown');
+  }
+
+  onClickClose(event) {
+    event.preventDefault();
+
+    const el = event.currentTarget;
+    const projectName = el.getAttribute('data-target');
+    const overlay = document.querySelector(`[data-project="${projectName}"]`);
+
+    overlay.classList.remove('is-shown');
+    
+    setTimeout(() => { overlay.scrollTop = 0; }, 750);
+  }
+}
+
+(function() { 
+  new Main();
 })();
